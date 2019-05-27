de974b2dcf100e8f0694d66e0f4d3003f499fb75
rammi0446 <rammi.0446@gmail.com>
2019-05-27 11:18:56 -0400

    R4:Red

--- test/TccTest.java
+++ test/TccTest.java
@@ -36,15 +36,35 @@ public class TccTest {
 
 	}
 	
-//	//R3:Zone 3
-//	@Test
-//	public void testBetweenZone() {
-//		Tcc t = new Tcc();
-//		String[] from = { "Don Mills" };
-//		String[] to = { "Finch" };
-//		double output = t.calculateTotal(from, to);
-//		assertEquals("3.0", String.valueOf(output));
-//
-//	}
+	//R3:Zone 3
+	@Test
+	public void testBetweenZone() {
+		Tcc t = new Tcc();
+		String[] from = { "Don Mills" };
+		String[] to = { "Finch" };
+		double output = t.calculateTotal(from, to);
+		assertEquals("3.0", String.valueOf(output));
+
+	}
+	
+	//R3:Zone 3
+		@Test
+		public void testTrip() {
+			Tcc t = new Tcc();
+			//First trip
+			String[] from1 = { "Finch" };
+			String[] to1 = { "Sheppard" };
+			double output1 = t.calculateTotal(from1, to1);
+			assertEquals("3.0", String.valueOf(output1));
+			//trip two
+			String[] from2 = { "Leslie" };
+			String[] to2 = { "Don Mills" };
+			double output2 = t.calculateTotal(from2, to2);
+			assertEquals("2.5", String.valueOf(output2));
+			//total cost
+			double total = output1 + output2;
+			assertEquals(5.50, total);
+
+		}
 
 }
