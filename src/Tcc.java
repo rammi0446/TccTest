25b1e9b1cf070c6984ad6af32a88f86b19128bb0
rammi0446 <rammi.0446@gmail.com>
2019-05-27 11:11:37 -0400

    R3:Refactor

--- src/Tcc.java
+++ src/Tcc.java
@@ -1,18 +1,26 @@
+import java.util.Arrays;
 
 public class Tcc {
 	public double calculateTotal(String[] from, String[] to)
 	{
 		String[] Zone1 = {"Don Mills","Leslie"};
 		String[] Zone2 = {"Sheppard","Finch"};
-		if(from[0] == Zone1[1] && to[0] == Zone1[0])
+		boolean containFromZone1 = Arrays.stream(Zone1).anyMatch(from[0]::equals);
+		boolean containtoZone1 = Arrays.stream(Zone1).anyMatch(to[0]::equals);
+
+		
+		boolean containFromZone2 = Arrays.stream(Zone2).anyMatch(from[0]::equals);
+		boolean containtoZone2 = Arrays.stream(Zone2).anyMatch(to[0]::equals);
+
+		if(containFromZone1 == true && containtoZone1 == true)
 		{
 			return 2.5;
 		}
-		else if(from[0] == Zone2[0] && to[0] == Zone2[1])
+		else if(containFromZone2 == true && containtoZone2 == true)
 		{
 			return 3.00;
 		}
-		else if(from[0] == Zone1[0] && to[0] == Zone2[1])
+		else if(containFromZone1 == true && containtoZone2 == true)
 		{
 			return 3.00;
 		}
